1e3113973f3179d057a96266982eeae6713c668f
mario-le92 <lqthinh92@gmail.com>
2013-06-28 16:49:02 +0700

    update chuyen sang php

---
+++ index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="css/css1.css">
+<link rel="stylesheet" type="text/css" href="css/css2.css">
+<script type="text/javascript" src="js/js1.js"></script>
+<script type="text/javascript" src="js/dropdowncontent.js"></script>
+<title>index</title>
+</head>
+
+<body>
+
+<header class="TheSameClass">
+
+</header>
+
+<div id="bbody" class="TheSameClass" >
+<hr /><br />
+<aside id="left">
+</aside>
+
+<aside id="right">
+
+<details>
+<summary>mẩu form</summary>
+<?php include("form.php"); ?>
+</details>
+
+<details>
+<summary>mẩu google search</summary>
+<?php include("google search.php"); ?>
+</details>
+
+<details>
+<summary>mẩu list</summary>
+<?php include("list.php"); ?>
+</details>
+
+<details>
+<summary>mẩu navbar</summary>
+<?php include("navbar.php"); ?>
+</details>
+
+<details>
+<summary>mẩu table</summary>
+<?php include("table.php"); ?>
+</details>
+
+<details>
+<summary>mẩu html5 support</summary>
+<?php include("html5 support.php"); ?>
+</details>
+
+<details>
+<summary>mẩu music picture video</summary>
+<?php include("music picture video.php"); ?>
+</details>
+
+</aside>
+<br /><hr />
+</div>
+
+<footer class="TheSameClass">
+All right reserved &#169; AngrysBirds
+</footer>
+
+</body>
+</html>
